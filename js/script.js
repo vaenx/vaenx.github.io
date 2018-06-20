9c3e77517f8701cc080f3cad2e8c0b277f2aa02c
Vanessa <vanessaveigavilela@gmail.com>
2018-06-20 23:35:00 +0100

    First Commit
    First commit - under construction

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){
+		Typed.new(".entry_typewritter", {
+			strings: ["Hello. I’m Vanessa, a UX Designer from Lisbon", "Last year I've been busy building Brixel with my team", "And researching about Design's role in the Attention Economy in my Master", "Basically, how design impacts user's choice and freewill", "And my time has been limited to design this website", "Check my online profiles for more info or give me a call ;)"],
+			typeSpeed: 40,
+      loop: false,
+		});
+	});
