6dc869677925059ceda6a23b8d24fd269210b7bc
Vanessa <vanessaveigavilela@gmail.com>
2018-06-21 17:10:14 +0100

    Contact and new links

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(){
 		Typed.new(".entry_typewritter", {
-			strings: ["Hello. I’m Vanessa, a UX Designer from Lisbon", "Last year I've been busy building Brixel with my team", "And researching about design impact in users' choices", "For more info check the below profiles ;)"],
+			strings: ["Hello. I’m Vanessa, a UX Designer from Lisbon", "Last year I've been busy building Brixel with my team", "And researching about design impact in users' choices", "Check my profiles for more info below."],
 			typeSpeed: 40,
       loop: true,
 		});
