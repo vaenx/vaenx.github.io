304bb8a45bc257aedb8cb8418b8f948ef76cf637
Vanessa <vanessaveigavilela@gmail.com>
2020-08-14 17:57:54 +0200

    Adding design process tab content with images

--- js/script.js
+++ js/script.js
@@ -5,3 +5,31 @@ document.addEventListener("DOMContentLoaded", function(){
       loop: false,
 		});
 	});
+
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(tab => {
+	tab.addEventListener('click', () => {
+		const target = document.querySelector(tab.dataset.tabTarget)
+		tabContents.forEach(tabContent => {
+			tabContent.classList.remove('active')
+	})
+		tabs.forEach(tab => {
+			tab.classList.remove('active')
+	})
+		tab.classList.add('active')
+		target.classList.add('active')
+	})
+})
+
+
+//----------tab change------------//
+
+// $(function() {
+//     $('.tabs_container').tabs_container();
+//
+//     $("#go-to-design-sprint").click(function() {
+//         $('.tabs_container').tabs_container('option', 'active', 3); //index 2 mean tab 3
+//     });
+// });
